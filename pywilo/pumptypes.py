00e4cb0e9251cc7343fda6e7743b8c16e0ade7ab
Christoph Schueler <cpu12.gems@googlemail.com>
2014-05-30 09:30:45 +0200

    Add 'pumptypes.py'.

---
+++ pywilo/pumptypes.py
@@ -0,0 +1,122 @@
+## WiloPLR - Communication with Wilo pumps.
+##
+## (C 2009-2013 by Christoph Schueler <github.com/Christoph2,
+##                                      cpu12.gems@googlemail.com>
+##
+##
+## All Rights Reserved
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; version 2 of the License.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+##
+## s. FLOSS-EXCEPTION.txt
+##
+#
+
+WILO_PT_TOP_E40_1_10_EP_DP           = 1
+WILO_PT_TOP_E50_1_7_EP_DP            = 2
+WILO_PT_TOP_E50_1_10_EP_DP           = 3
+WILO_PT_TOP_E65_1_10_EP_DP           = 4
+WILO_PT_TOP_E80_1_10_EP_DP           = 5
+WILO_PT_TOP_E80_1_10_N_EP_DP         = 6
+WILO_PT_TOP_E25_1_7_EP               = 7
+WILO_PT_TOP_E30_1_10_EP              = 8
+WILO_PT_TOP_E40_1_4_EP               = 9
+WILO_PT_TOP_E50_1_6_EP_DP            = 10
+WILO_PT_TOP_E30_1_7_EP_DP            = 11
+WILO_PT_TOP_E40_1_10_IR_EP_DP        = 12
+WILO_PT_TOP_E50_1_7_IR_EP_DP         = 13
+WILO_PT_TOP_E50_1_10_IR_EP_DP        = 14
+WILO_PT_TOP_E65_1_10_IR_EP_DP        = 15
+WILO_PT_TOP_E80_1_10_IR_EP_DP        = 16
+WILO_PT_TOP_E40_1_7_IR_EP_DP         = 17
+WILO_PT_TOP_E100_1_10_IR_EP_DP       = 18
+
+WILO_PT_IP_E32_5_28_EP_DP            = 20
+WILO_PT_IP_E40_2_12_EP_DP            = 21
+WILO_PT_IP_E40_5_28_EP_DP            = 23
+WILO_PT_IP_E50_2_12_EP_DP            = 24
+WILO_PT_IP_E50_5_28_EP_DP            = 26
+WILO_PT_IP_E65_2_15_EP_DP            = 27
+WILO_PT_IP_E65_4_20_EP_DP            = 29
+WILO_PT_IP_E80_2_15_EP_DP            = 30
+WILO_PT_IP_E80_4_20_EP_DP            = 31
+
+WILO_PT_IL_E50_10_36_EP_IL_DP_DL     = 32
+WILO_PT_IL_E65_6_24_EP_IL_DP_DL      = 33
+WILO_PT_IL_E80_5_22_EP_IL_DP_DL      = 34
+WILO_PT_IL_E40_9_39_EP_IL_DP_DL      = 35
+WILO_PT_IP_E50_140_3_2_EP_DP         = 36
+WILO_PT_IP_E80_130_3_2_EP_DP         = 37
+WILO_PT_IP_E40_160_4_2_EP_DP         = 38
+WILO_PT_IP_E65_140_4_2_EP_DP         = 39
+
+WILO_PT_STRATOS_30_1_12_EP           = 40
+WILO_PT_STRATOS_40_1_8_ EP_DP        = 41
+WILO_PT_STRATOS_50_1_8_EP_DP         = 42
+WILO_PT_STRATOS_32_1_12_EP_DP        = 43
+WILO_PT_STRATOS_40_1_12_EP_DP        = 44
+WILO_PT_STRATOS_50_1_12_EP_DP        = 45
+WILO_PT_STRATOS_50_1_9_EP_DP         = 46
+WILO_PT_STRATOS_65_1_9_EP            = 47
+WILO_PT_STRATOS_65_1_12_EP_DP        = 48
+WILO_PT_STRATOS_80_1_12_EP_DP        = 49
+WILO_PT_STRATOS_100_1_12_EP_DP       = 50
+WILO_PT_STRATOS_25_1_6_EP            = 51
+WILO_PT_STRATOS_30_1_6_EP            = 52
+WILO_PT_STRATOS_40_1_4_EP            = 53
+WILO_PT_STRATOS_25_1_8_EP            = 54
+WILO_PT_STRATOS_30_1_8 _EP_DP        = 55
+WILO_PT_STRATOS_25_1_12_EP           = 56
+
+WILO_PT_MHIE205                      = 60
+WILO_PT_MHIE403                      = 61
+WILO_PT_MVIE205                      = 62
+WILO_PT_MVIE403                      = 63
+WILO_PT_MHIE405                      = 64
+WILO_PT_MHIE803                      = 65
+WILO_PT_MHIE1602                     = 66
+WILO_PT_MVIE208                      = 67
+WILO_PT_MVIE406                      = 68
+WILO_PT_MVIE803                      = 69
+WILO_PT_MVIE1602                     = 70
+WILO_PT_MHIE806                      = 71
+WILO_PT_MHIE1603                     = 72
+WILO_PT_MVIE214                      = 73
+WILO_PT_MVIE410                      = 74
+WILO_PT_MVIE806                      = 75
+WILO_PT_MVIE1603                     = 76
+WILO_PT_MVISE206                     = 77
+WILO_PT_MVISE404                     = 78
+WILO_PT_MVISE406                     = 79
+WILO_PT_MVISE803                     = 80
+WILO_PT_MVISE210                     = 81
+WILO_PT_MVISE410                     = 82
+WILO_PT_MVISE806                     = 83
+WILO_PT_SHCE20                       = 84
+
+WILO_PT_MVIE414                      = 90
+WILO_PT_MVIE808                      = 91
+WILO_PT_MVIE1605                     = 92
+WILO_PT_MVIE3202                     = 93
+WILO_PT_MVIE811                      = 94
+WILO_PT_MVIE1607                     = 95
+WILO_PT_MVIE3203                     = 96
+WILO_PT_MVIE5202                     = 97
+
+WILO_PT_IP_E32_100_0_55_2            = 101
+WILO_PT_IP_E32_110_0_75_2            = 102
+WILO_PT_IP_E40_120_1_5_2             = 103
+WILO_PT_IP_E40_130_2_2_2             = 104
+WILO_PT_IP_E50_130_2_2_2             = 105
+
